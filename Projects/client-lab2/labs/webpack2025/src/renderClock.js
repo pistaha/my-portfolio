587c22d12e152fc3afbdc934cce6bb1c0ceef1d6
pistaha <kataracta.pq@gmail.com>
2026-04-25 15:32:00 +0300

    Reduce stage 2 to assignment-only files

--- Projects/client-lab2/labs/webpack2025/src/renderClock.js
+++
@@ -1,5 +0,0 @@
-import { DateTime } from "luxon";
-
-export function getCurrentDateTime() {
-  return DateTime.local().setLocale("ru").toFormat("dd.MM.yyyy HH:mm:ss");
-}
--- Projects/client-lab2/labs/webpack2025/webpack.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-  },
-};
